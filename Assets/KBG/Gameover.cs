574587c668349e5c9b479a8da4581210c3a2e45d
Dorran13 <dorani0918@naver.com>
2025-04-28 16:22:21 +0900

    hp0 UI구현
    플레이어 사망씬

---
+++ Assets/KBG/Gameover.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameover : MonoBehaviour
+{
+    bool isCleared = false;
+    bool gameOver = false;
+    int hp;
+    int killCount;
+    int timer;
+    void Update()
+    {
+        if (gameOver == true && isCleared == false)
+        {
+            //depeat
+        }
+        if (gameOver == true && isCleared == true)
+        {
+            //victory
+        }
+        if (gameOver == false)
+        {
+            if (hp <= 0)
+            {
+                gameOver = true;
+            }
+            if (killCount == 100)
+            {
+                gameOver = true;
+                isCleared = true;
+            }
+            if (timer == 0)
+            {
+                gameOver = true;
+                isCleared = true;
+            }
+        }
+        
+    }
+}
